a4303e368d23bcbf0d897fcd66c29cac05539495
Daniel Jowett <daniel@jowettenterprises.com>
2018-07-08 22:29:45 +0100

    Add validation on image type

--- ftw/logo/manual_override.py
+++ ftw/logo/manual_override.py
@@ -9,8 +9,25 @@ from Products.Five.browser import BrowserView
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 import transaction
 from zope import schema
+from zope.interface import Invalid
 
 from ftw.logo import _
+
+def svg_file_only(value):
+    if value.contentType != 'image/svg+xml':
+        raise Invalid(
+            u"This image must be a SVG file " +
+            u"({} supplied)".format(value.contentType)
+        )
+    return True
+
+def png_file_only(value):
+    if value.contentType != 'image/png':
+        raise Invalid(
+            u"This image must be a PNG file " +
+            u"({} supplied)".format(value.contentType)
+        )
+    return True
 
 class IManualOverrides(model.Schema):
 
@@ -25,16 +42,19 @@ class IManualOverrides(model.Schema):
         description = _(u"Overriding the base logo will generate an override "
                         u"for BOTH logos below if not already set."),
         required=False,
+        constraint=svg_file_only,
     )
 
     logo_LOGO = NamedBlobImage(
         title = _(u"Standard (desktop) logo (PNG)"),
         required=False,
+        constraint=png_file_only,
     )
 
     logo_MOBILE_LOGO = NamedBlobImage(
         title = _(u"Mobile logo (PNG)"),
         required=False,
+        constraint=png_file_only,
     )
 
     icon_BASE = NamedBlobImage(
@@ -42,43 +62,50 @@ class IManualOverrides(model.Schema):
         description = _(u"Overriding the base icon will generate an "
                         u"override for ALL icons below if not already set."),
         required=False,
+        constraint=svg_file_only,
     )
 
     icon_APPLE_TOUCH_ICON = NamedBlobImage(
         title = _(u"Apple touch icon"),
         required=False,
+        constraint=png_file_only,
     )
 
     icon_FAVICON_32X32 = NamedBlobImage(
         title = _(u"Favicon 32x32"),
         required=False,
+        constraint=png_file_only,
     )
 
     icon_FAVICON_16X16 = NamedBlobImage(
         title = _(u"Favicon 16x16"),
         required=False,
+        constraint=png_file_only,
     )
 
     icon_MSTILE_150X150 = NamedBlobImage(
         title = _(u"Mstile icon 150x150"),
         required=False,
+        constraint=png_file_only,
     )
 
     icon_ANDROID_192X192 = NamedBlobImage(
         title = _(u"Android icon 192x192"),
         required=False,
+        constraint=png_file_only,
     )
 
     icon_ANDROID_512X512 = NamedBlobImage(
         title = _(u"Android icon 512x512"),
         required=False,
+        constraint=png_file_only,
     )
 
     icon_FAVICON = NamedBlobImage(
         title = _(u"Favicon"),
         required=False,
+        constraint=png_file_only,
     )
-
 
 class CreateOverridesIfReqdForm(BrowserView):
     """
