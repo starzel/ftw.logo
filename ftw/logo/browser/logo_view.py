1a3811d15bed5baf605ee7f99cbf8a45abbc9428
Daniel Jowett <daniel@jowettenterprises.com>
2018-07-09 20:06:45 +0100

    Use uploaded images from the new form to override zcml values in logo view

--- ftw/logo/browser/logo_view.py
+++ ftw/logo/browser/logo_view.py
@@ -3,8 +3,11 @@ from ftw.logo.converter import flatten_scales
 from ftw.logo.interfaces import IIconConfig
 from ftw.logo.interfaces import ILogo
 from ftw.logo.interfaces import ILogoConfig
+from ftw.logo.manual_override import OVERRIDES_FIXED_ID
 from ftw.logo.StringIOStreamIterator import StringIOStreamIterator
 from plone.app.layout.globals.interfaces import IViewView
+from plone.app.layout.navigation.interfaces import INavigationRoot
+from plone.namedfile.browser import DisplayFile
 from Products.Five.browser import BrowserView
 from zExceptions import BadRequest
 from zExceptions import NotFound
@@ -30,6 +33,7 @@ class LogoView(BrowserView):
 
     def publishTraverse(self, request, name):
         if self.config is None and name in CONFIGS:
+            self.config_name = name
             self.config = CONFIGS[name]
             return self
         elif self.config and name in flatten_scales(SCALES):
@@ -41,6 +45,31 @@ class LogoView(BrowserView):
     def __call__(self):
         if not self.config or not self.scale:
             raise BadRequest()
+        return (self.get_dx_overridden_image() or
+                self.get_zcml_configured_image())
+
+    def get_dx_overridden_image(self):
+        # TODO this check not nec - remove
+        if not INavigationRoot.providedBy(self.context):
+            return None
+        overridesItem = self.context.get(OVERRIDES_FIXED_ID)
+        if overridesItem is None:
+            return None
+        # check if requested scale has been overridden and return it
+        field_name = '{}_{}'.format(self.config_name, self.scale)
+        field = getattr(overridesItem, field_name)
+        if not field:
+            # check if base logo/icon  has been overridden, then transform it and return it
+            base_field_name = '{}_BASE'.format(self.scale)
+            field = getattr(overridesItem, base_field_name)
+            # TODO transform
+        if not field:
+            return None
+
+        display_file = DisplayFile(overridesItem, self.request).publishTraverse(self.request, field_name or base_field_name)
+        return display_file()
+
+    def get_zcml_configured_image(self):
         config = getMultiAdapter(
             (self.context, self.request), ILogo).get_config(self.config)
         scale = config.get_scale(self.scale)
