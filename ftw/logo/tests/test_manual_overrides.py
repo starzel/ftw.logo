e6c1db9da55f35bca074ac6f2ab2fda6b6c29adf
Daniel Jowett <daniel@jowettenterprises.com>
2018-08-21 01:40:31 +0100

    Test override form

--- ftw/logo/tests/test_manual_overrides.py
+++ ftw/logo/tests/test_manual_overrides.py
@@ -4,9 +4,13 @@ from ftw.logo.manual_override import OVERRIDES_FIXED_ID
 from ftw.logo.tests import FunctionalTestCase
 from ftw.testbrowser import browsing
 import os
+from wand.color import Color
+from wand.exceptions import CorruptImageError
+from wand.image import Image
+
 
 source_path = os.path.join(os.path.dirname(__file__), 'fixtures')
-custom_svg = os.path.join(source_path, 'custom.svg')
+red_svg = os.path.join(source_path, 'red.svg')
 green_png = os.path.join(source_path, 'green.png')
 
 
@@ -47,21 +51,54 @@ class TestManualOverrides(FunctionalTestCase):
             browser.login().visit(folder, view='@@logo-and-icon-overrides')
 
 
+    def verify_correct_image(self, browser, view, expected_format, expected_colour=None):
+        # We logout to test images are *publicly* visible
+        browser.logout()
+        browser.visit(self.portal, view=view)
+        self.assertEqual(200, browser.status_code)
+        try:
+            im = Image(blob=browser.contents, format=expected_format)
+        except CorruptImageError:
+            self.fail("Image is incorrect format - expected {}".format(expected_format))
+        if (expected_colour):
+            # Determine the source (SVG, PNG) by using the colour
+            # Test that 90% of image is expected_colour
+            num_pixels = im.height * im.width
+            self.assertGreater(im.histogram[Color(expected_colour)], num_pixels * 0.9)
+        return im
+
+
     @browsing
     def test_overrides(self, browser):
-        # TODO test adding overrides
+        # test adding overrides
         self.grant('Site Administrator')
         browser.login().visit(self.portal, view='@@logo-and-icon-overrides')
         self.assertEqual(200, browser.status_code)
-        with open(custom_svg) as svg_file:
+        with open(red_svg) as svg_file:
             browser.fill({'SVG base logo': svg_file}).submit()
 
-        # TODO conversion to PNG
-        # Test view of PNG
-        # TODO test fallback if overrides not set
-        # TODO test overrides *publicly* visible too
+        # Check SVG image is set
+        img = self.verify_correct_image(browser, '@@logo/logo/BASE', 'svg', 'red')
+        # Check conversion to PNG
+        img = self.verify_correct_image(browser, '@@logo/logo/MOBILE_LOGO', 'png', 'red')
+        self.assertEqual(50, img.height)
+        # Check ZCML 'bypass'
+        img = self.verify_correct_image(browser, '@@logo/z/logo/MOBILE_LOGO', 'png')
+        # TODO verify base SVG is shown
+        # TODO verify icons are unchanged
 
-        # Test ZCML 'bypass'
+        # Test PNG override
+        browser.login().visit(self.portal, view='@@logo-and-icon-overrides')
+        with open(green_png) as png_file:
+            browser.fill({'Mobile logo (PNG)': png_file}).submit()
+        img = self.verify_correct_image(browser, '@@logo/logo/MOBILE_LOGO', 'png', '#0f0')
+        # Note dimensions of a PNG override are not enforced
+        # Check ZCML 'bypass'
+        img = self.verify_correct_image(browser, '@@logo/z/logo/MOBILE_LOGO', 'png')
+        # TODO verify base SVG is shown
+
+        # TODO test icons similar to above
+
 
 
     @browsing
@@ -73,6 +110,6 @@ class TestManualOverrides(FunctionalTestCase):
         self.assertIn('This image must be a SVG file (image/png supplied)', browser.contents)
 
         browser.login().visit(self.portal, view='@@logo-and-icon-overrides')
-        with open(custom_svg) as svg_file:
+        with open(red_svg) as svg_file:
             browser.fill({'Apple touch icon': svg_file}).submit()
         self.assertIn('This image must be a PNG file (image/svg+xml supplied)', browser.contents)
